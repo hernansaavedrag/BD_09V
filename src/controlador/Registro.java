80e6210c43a492a2ecfed20f528546a9fad56712
Hernan Saavedra <hernan.saavedra@gmail.com>
2021-11-19 23:35:32 -0300

    subiendo CRUD

--- src/controlador/Registro.java
+++ src/controlador/Registro.java
@@ -1,12 +1,169 @@
-
 package controlador;
+
+import bd.Conexion;
+import java.util.Date;
+import modelo.Libro;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Hernan
  */
 public class Registro {
-    
-    
-    
+
+    public boolean agregar(Libro libro) {
+
+        Date date;
+        try {
+
+            Conexion con = new Conexion();
+            Connection cnx = con.obtenerConexion();
+
+            date = libro.getPublicacion();
+
+            String query = "INSERT INTO libro(titulo,autor,publicacion,precio,disponible)VALUES(?,?,?,?,?)";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setString(1, libro.getTitulo());
+            stmt.setString(2, libro.getAutor());
+            stmt.setDate(3, new java.sql.Date(date.getTime()));
+            stmt.setInt(4, libro.getPrecio());
+            stmt.setBoolean(5, libro.isDisponible());
+
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error en SQL al agregar libro - Clase Registro" + e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean actualizar(Libro libro) {
+
+        Date date;
+        try {
+
+            Conexion con = new Conexion();
+            Connection cnx = con.obtenerConexion();
+
+            date = libro.getPublicacion();
+
+            String query = "UPDATE libro set titulo = ?,autor=?,publicacion =?,precio=?,disponible=?  WHERE idlibro =?";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setString(1, libro.getTitulo());
+            stmt.setString(2, libro.getAutor());
+            stmt.setDate(3, new java.sql.Date(date.getTime()));
+            stmt.setInt(4, libro.getPrecio());
+            stmt.setBoolean(5, libro.isDisponible());
+            stmt.setInt(6, libro.getIdLibro());
+
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error en SQL al actualizar libro - Clase Registro" + e.getMessage());
+            return false;
+        }
+
+    }
+
+    public boolean eliminar(int idLibro) {
+
+        try {
+
+            Conexion con = new Conexion();
+            Connection cnx = con.obtenerConexion();
+
+            String query = "DELETE FROM libro WHERE idlibro=?";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setInt(1, idLibro);
+
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error en SQL al eliminar libro - Clase Registro" + e.getMessage());
+            return false;
+        }
+    }
+
+    public Libro buscarPorId(int idLibro) {
+
+        Libro libro = new Libro();
+        try {
+            Conexion con = new Conexion();
+            Connection cnx = con.obtenerConexion();
+
+            String query = "SELECT idlibro,titulo,autor,publicacion,precio,disponible FROM libro WHERE idlibro = ?";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setInt(1, idLibro);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                libro.setIdLibro(rs.getInt("idlibro"));
+                libro.setTitulo(rs.getString("titulo"));
+                libro.setAutor(rs.getString("autor"));
+                libro.setPublicacion(rs.getDate("publicacion"));
+                libro.setPrecio(rs.getInt("precio"));
+                libro.setDisponible(rs.getBoolean("disponible"));
+
+            }
+            rs.close();
+            stmt.close();
+            cnx.close();
+
+        } catch (SQLException e) {
+            System.out.println("Error SQL al listar libro por ID " + e.getMessage());
+        }
+        return libro;
+    }
+
+    public List<Libro> buscarTodos() {
+
+        List<Libro> lista = new ArrayList<>();
+        try {
+            Conexion con = new Conexion();
+            Connection cnx = con.obtenerConexion();
+
+            String query = "SELECT idlibro,titulo,autor,publicacion,precio,disponible FROM libro order by titulo";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Libro libro = new Libro();
+                libro.setIdLibro(rs.getInt("idlibro"));
+                libro.setTitulo(rs.getString("titulo"));
+                libro.setAutor(rs.getString("autor"));
+                libro.setPublicacion(rs.getDate("publicacion"));
+                libro.setPrecio(rs.getInt("precio"));
+                libro.setDisponible(rs.getBoolean("disponible"));
+
+                lista.add(libro);
+            }
+            rs.close();
+            stmt.close();
+            cnx.close();
+
+        } catch (SQLException e) {
+            System.out.println("Error SQL al listar libro " + e.getMessage());
+        }
+        return lista;
+    }
+
 }
